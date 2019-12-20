f51c57710846f6ab57bca9f9681a5349e2b6aef9
Johan Lindfors <Johan.Lindfors@coderox.se>
2019-12-20 12:11:50 +0100

    Step 3 - Apple and snake

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
+// use std::collections::VecDeque;
+// use tetra::math::Vec2;
 
 //const FRAMES_PER_SECOND: f64 = 15.0;
 const SPRITE_SIZE: i32 = 20;
 const SCREEN_SIZE: i32 = 20;
 //const INITIAL_TAIL: usize = 5;
 
-struct GameState;
+struct Apple {
+    // position: Vec2<i32>,
+}
+
+impl Apple {
+    fn new() -> tetra::Result<Self> {
+        Ok(Self {
+            // position: Vec2::new(0,0)
+        })
+    }
+}
+
+struct Snake {
+    // position: Vec2<i32>,
+    // direction: Vec2<i32>,
+    // trail: VecDeque<Vec2<i32>>,
+    // tail: usize,
+}
+
+impl Snake {
+    fn new() -> tetra::Result<Self> {
+        Ok(Self {
+            // position: Vec2::new(0,0),
+            // direction: Vec2::new(0,0),
+            // trail: VecDeque::new(),
+            // tail: INITIAL_TAIL
+        })
+    }
+}
+
+struct GameState {
+    apple: Apple,
+    snake: Snake,
+}
+
+impl GameState {
+    fn new(_ctx: &mut Context) -> tetra::Result<Self> {
+        Ok(Self{
+            apple: Apple::new()?,
+            snake: Snake::new()?
+        })
+    }
+}
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
@@ -22,5 +66,5 @@ fn main() -> tetra::Result {
     ContextBuilder::new("Snake!", width, height)
         .quit_on_escape(true)
         .build()?
-        .run(|_| Ok(GameState))
+        .run(GameState::new)
 }
